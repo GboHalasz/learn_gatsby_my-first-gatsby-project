b59dd35600f5bfae17586dcc0ec0df146dd72055
GboHalasz <hal2297dev@gmail.com>
2023-10-09 13:57:53 +0200

    Added clifford.jpg and typings.d.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -9,6 +10,10 @@ const IndexPage = () => {
       <Layout pageTitle="Home Page">
         <h1>Welcome to my Gatsby site!</h1>
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
       </Layout>
     </main>
   );
